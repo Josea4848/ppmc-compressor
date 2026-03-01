767902eb8df91645c649e2c29c91a59a9fec4d32
Matheus <mathfelpessoal@gmail.com>
2026-03-01 20:00:45 -0300

    começa do k mais alto, correção da codificação do RO

--- src/main.cpp
+++ src/main.cpp
@@ -11,10 +11,26 @@
 #include "FrequencyTable.hpp"
 
 
-void clear_freqs(std::vector<uint32_t> &freqs) {
-    for(size_t i = 0; i < freqs.size(); i++) {
-        freqs[i] = 0;         
+void print_context(
+    std::vector<std::map<std::string, std::map<std::string, uint32_t>>> &contextual_tables
+) {
+    for(size_t i = 0; i < contextual_tables.size(); i++) {
+        if(contextual_tables[i].size() == 0) {
+            continue;
+        }
+        std::cout << "K = " << i << std::endl;
+        
+        std::map<std::string, std::map<std::string, uint32_t>>::iterator it;
+        for(it = contextual_tables[i].begin(); it != contextual_tables[i].end(); it++) {
+            std::cout << "Contexto " << it->first << std::endl;
+            std::map<std::string, uint32_t>::iterator it2;
+            for(it2 = it->second.begin(); it2 != it->second.end(); it2++) {
+                std::cout << "Símbolo: " << it2->first << " | Contador: " << it2->second << std::endl;
+            }
+            std::cout << std::endl; 
+        }
     }
+    std::cout << "-------------------------------------------" << std::endl;
 }
 
 
@@ -34,15 +50,52 @@ void update_context(
         // contexto
         std::string ctx_substr = msg.substr(i-j, j);
         contextual_tables[j][ctx_substr][std::string(1, byte)] += 1;
-        contextual_tables[j][ctx_substr][""] += 1; // se começar com 0, dá para deixar assim
+        if(contextual_tables[j][ctx_substr][std::string(1, byte)] == 1) {
+            contextual_tables[j][ctx_substr][""] += 1; // se começar com 0, dá para deixar assim
+        }
     }
+    
+    // print_context(contextual_tables);
 }
 
-int main() {
-    int k = 3;
-    std::string msg = "abracadabra";
+int main(int argc, char* argv[]) {
+
+    if (argc < 3) {
+        std::cout << "Uso: ./compressor <arquivo> <k>\n";
+        return 1;
+    }
+
+    std::string filename = argv[1];
+    int k;
+    if(argc > 2) {
+        k = std::stoi(argv[2]);
+    } else {
+        k = 3;
+    }
+
+    std::ifstream inFile(filename, std::ios::binary);
+
+    if (!inFile) {
+        std::cout << "Erro ao abrir arquivo.\n";
+        return 1;
+    }
+
+    std::string msg{
+        std::istreambuf_iterator<char>(inFile),
+        std::istreambuf_iterator<char>()};
+
+    inFile.close();
+
+    if (msg.empty()) {
+        std::cout << "Arquivo vazio.\n";
+        return 1;
+    }
+
+    std::cout << "Bytes lidos: " << msg.size() << "\n";
+    getchar();
 
     std::vector<std::map<std::string, std::map<std::string, uint32_t>>> contextual_tables(k);
+    std::map<std::string, std::map<std::string, uint32_t>>::iterator ctx_table;
     std::vector<uint32_t> unkown_symbols(256, 1);
     std::vector<uint32_t> freqs(257, 0);
 
@@ -56,79 +109,95 @@ int main() {
     BitOutputStream bitOut(outFile);
     ArithmeticEncoder encoder(32, bitOut);
     
-    contextual_tables[0][""][""] = 0;
     for(int i = 0; i < static_cast<int>(msg.size()); i++) {
         std::vector<bool> excluded_symbols(256, false);
         std::map<std::string, uint32_t>::iterator it;
         unsigned char byte = msg[i];
+        bool encoded = false;
+
         std::cout << "byte: " << byte << std::endl;
 
-        if ( unkown_symbols[ static_cast<int>(msg[i]) ] ) {
-            SimpleFrequencyTable freqTable(unkown_symbols);
+        for(int j = k-1; j >= 0; j--) {
+            if(i < j) {
+                continue;
+            }
 
-            encoder.write(freqTable, byte);
+            // contexto
+            std::string ctx_substr = msg.substr(i-j, j);
 
-            unkown_symbols[static_cast<int>(msg[i])] = 0; 
-        } else {
-            for(int j = k-1; j >= 0; j--) {
-                if(i < j) {
-                    continue;
-                }
+            // procurando contexto com k símbolos na tabela k
+            ctx_table = contextual_tables[j].find(ctx_substr);
 
-                // contexto
-                std::string ctx_substr = msg.substr(i-j, j);
+            if(ctx_table == contextual_tables[j].end()) {
+                continue;
+            }
 
-                // procurando contexto com k símbolos na tabela k
-                std::map<std::string, std::map<std::string, uint32_t>>::iterator ctx_table;
-                ctx_table = contextual_tables[j].find(ctx_substr);
+            // procurando se existe o símbolo no contexto
+            std::map<std::string, uint32_t>::iterator iter_symbol_counter = ctx_table->second.find(std::string(1, msg[i]));
 
-                if(ctx_table == contextual_tables[j].end()) {
-                    continue;
-                }
-
-                // procurando se existe o símbolo no contexto
-                std::map<std::string, uint32_t>::iterator iter_symbol_counter = ctx_table->second.find(std::string(1, msg[i]));
-
-                // caso não encontre, vê quais símbolos não devem ser considerados nas 
-                // próximas tabelas (mecânismo de exclusão) e codifica um ro
-                if(iter_symbol_counter == ctx_table->second.end()){
-                    for(it = ctx_table->second.begin(); it != ctx_table->second.end(); it++) {
-                        if(it->first.compare("") != 0) {
-                            excluded_symbols[static_cast<int>(it->second)] = true; // adicionando na tabela para exclusão
-                            freqs[static_cast<uint32_t>(it->first[0])] = it->second; // montando tabela para codificar um ro 
-                        } else {
-                            freqs[256] = it->second; // montando tabela para codificar um ro 
-                        }
-                    }
-
-                    SimpleFrequencyTable freqTable(freqs);
-                    encoder.write(freqTable, byte);
-
-                    // limpa freqs para a próxima tabela
-                    clear_freqs(freqs);
-                    continue;
-                }
+            // caso não encontre, vê quais símbolos não devem ser considerados nas 
+            // próximas tabelas (mecânismo de exclusão) e codifica um ro
+            if(iter_symbol_counter == ctx_table->second.end()) {
                 
                 for(it = ctx_table->second.begin(); it != ctx_table->second.end(); it++) {
-                    if(it->first.compare("") == 0) {
-                        freqs[256] = it->second;
-                        continue;
+                    if(it->first.compare("") != 0) {
+                        int pos = static_cast<int>(it->first[0]);
+                        excluded_symbols[pos] = true; // adicionando na tabela para exclusão
+                        freqs[pos] = it->second; // montando tabela para codificar o ro 
+                    } else {
+                        freqs[256] = it->second; // montando tabela para codificar o ro 
                     }
-
-                    int symbol = static_cast<uint32_t>(it->first[0]);
-                    if( excluded_symbols[symbol] ) {
-                        continue;
-                    }
-
-                    freqs[symbol] = it->second;
                 }
 
                 SimpleFrequencyTable freqTable(freqs);
-                encoder.write(freqTable, byte);
+                encoder.write(freqTable, 256);
 
-                // limpa freqs para o próximo símbolo 
-                clear_freqs(freqs);
+                // limpa freqs para a próxima tabela
+                for(it = ctx_table->second.begin(); it != ctx_table->second.end(); it++) {
+                    if(it->first.compare("") != 0) {
+                        freqs[static_cast<int>(it->first[0])] = 0; // montando tabela para codificar um ro 
+                    } else {
+                        freqs[256] = 0; // montando tabela para codificar um ro 
+                    }
+                }
+
+                continue;
             }
+        
+            for(it = ctx_table->second.begin(); it != ctx_table->second.end(); it++) {
+                if(it->first.compare("") == 0) {
+                    freqs[256] = it->second;
+                    continue;
+                }
+
+                int symbol = static_cast<uint32_t>(it->first[0]);
+                if( excluded_symbols[symbol] ) {
+                    continue;
+                }
+
+                freqs[symbol] = it->second;
+            }
+
+            SimpleFrequencyTable freqTable(freqs);
+            encoder.write(freqTable, byte);
+
+            // limpa freqs para o próximo símbolo 
+            for(it = ctx_table->second.begin(); it != ctx_table->second.end(); it++) {
+                if(it->first.compare("") == 0) {
+                    freqs[256] = 0;
+                    continue;
+                }
+
+                freqs[static_cast<int>(it->first[0])] = 0;
+            }
+            encoded = true;
+            break;
+        }
+
+        if(!encoded) {
+            SimpleFrequencyTable freqTable(unkown_symbols);
+            encoder.write(freqTable, byte);
+            unkown_symbols[static_cast<int>(msg[i])] = 0;
         }
 
         update_context(i, k, contextual_tables, byte, msg);
